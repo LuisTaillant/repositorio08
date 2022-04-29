1be1b0662da25781b7775049fc14b3786942ad28
Luis Taillant <luis.marcelo.taillant@gmail.com>
2022-04-29 20:45:59 -0300

    DesafioClase11

---
+++ DesafioClase11/back/db/createDocs.js
@@ -0,0 +1,151 @@
+/* --------------------------------- CREATE --------------------------------- */
+import "./db.js";
+import { ChatMock } from "../models/chat.mock.model.js"
+import generateChats from "../utils/chat.utils.js"
+// import { ProductModel } from "../models/product.model.js";
+import { ChatModel } from "../models/chat.model.js";
+
+
+
+// Algunos chats a mano para repetir author
+const chat1 = {
+  author: {
+        id: 'luis@gmail.com', 
+        nombre: 'Luis', 
+        apellido: 'Taillant', 
+        alias: 'Lucho',
+        avatar: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png'
+    },
+  text: "Hola cabeza!",
+  created_at: new Date().toLocaleString()
+}
+
+const chat2 = {
+  author: {
+        id: 'luis@gmail.com', 
+        nombre: 'Luis', 
+        apellido: 'Taillant', 
+        alias: 'Lucho',
+        avatar: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png'
+    },
+  text: "Chau pichu!",
+  created_at: new Date().toLocaleString()
+}
+
+const chat3 = {
+  author: {
+        id: 'cele@gmail.com', 
+        nombre: 'Celeste', 
+        apellido: 'Oviedo', 
+        alias: 'Cele',
+        avatar: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'
+    },
+  text: "Hola amor",
+  created_at: new Date().toLocaleString()
+}
+
+const chat4=generateChats()
+
+export default async function createDocs() {
+  try {
+    //const prods = await ProductModel.create([prod1, prod2, prod3,prod4,prod5,prod6, prod7, prod8]);
+    //const chats = await ChatModel.create([chat1, chat2]);
+    const chatMocks = await ChatModel.create([chat1, chat2, chat3, chat4]);
+    console.log(chatMocks);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// const prod1 = {
+//   nombre: "Notebook HP 348 G7 14 HD Intel i5",
+//   tipo: "notebook",
+//   precio: 1700,
+//   imagen: "https://www.pcfactory.cl/public/foto/43833/1.jpg",
+//   stock: 12,
+//   quantity: 0,
+//   total: null
+// };
+// const prod2 = {
+//   nombre: "Notebook HP Gamer Pavilion Intel i5",
+//   tipo: "notebook gamer",
+//   precio: 2100,
+//   imagen: "https://www.pcfactory.cl/public/foto/38802/1.jpg",
+//   stock: 13,
+//   quantity: 0,
+//   total: null
+// };
+// const prod3 = {
+//   nombre: "Notebook Acer Gamer Nitro 5 Intel i5",
+//   tipo: "notebook",
+//   precio: 1700,
+//   imagen: "https://www.pcfactory.cl/public/foto/38217/1.jpg",
+//   stock: 4,
+//   quantity: 0,
+//   total: null
+// }
+
+// const prod4 = {
+//   nombre: "Notebook HP 240 G8 14 HD Celeron",
+//   tipo: "notebook",
+//   precio: 1700,
+//   imagen: "https://www.pcfactory.cl/public/foto/43505/1.jpg",
+//   stock: 13,
+//   quantity: 0,
+//   total: null
+// }
+
+// const prod5 = {
+//   nombre: "Notebook HP 2en1 Spectre x360 13.5 FHD Intel Core i5",
+//   tipo: "notebook",
+//   precio: 1700,
+//   imagen: "https://www.pcfactory.cl/public/foto/42286/1.jpg",
+//   stock: 12,
+//   quantity: 0,
+//   total: null
+// }
+
+// const prod6 = {
+//   nombre: "Smartphone Galaxy A12 128GB",
+//   tipo: "smartphone",
+//   precio: 1900,
+//   imagen: "https://www.pcfactory.cl/public/foto/42835/1.jpg",
+//   stock: 5,
+//   quantity: 0,
+//   total: null
+// }
+
+// const prod7 = {
+//    nombre: "Smartphone Moto E7 32GB",
+//   tipo: "smartphone",
+//   precio: 2100,
+//   imagen: "https://www.pcfactory.cl/public/foto/41618/1.jpg",
+//   stock: 3,
+//   quantity: 0,
+//   total: null
+// }
+
+// const prod8 = {
+//    nombre: "Smartphone Nokia 3.4 64GB",
+//   tipo: "smartphone",
+//   precio: 2000,
+//   imagen: "https://www.pcfactory.cl/public/foto/44961/1.jpg",
+//   stock: 14,
+//   quantity: 0,
+//   total: null
+// }
+
+// const chat1 = {
+//   author: 'luis@gmail',
+//   text: 'Pepe',
+//   created_at: new Date().toLocaleString()
+// }
+
+// const chat2 = {
+//   author: 'cele@gmail',
+//   text: 'Hi Luis',
+//   created_at: new Date().toLocaleString()
+// }
+
+
+
---
+++ DesafioClase11/back/server.js
@@ -0,0 +1,95 @@
+//Requerimientos
+import express from "express";
+import cors from "cors"
+import http from 'http'
+import { Server } from 'socket.io'
+import util from 'util'
+
+// Rutas
+import routerProd from "./routes/product.routes.js";
+import routerCart from "./routes/cart.routes.js"
+import routerProdTest from "./routes/mock.routes.js"
+
+const app = express();
+const server = http.createServer(app);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+})
+
+//Uso de rutas-------------------------------------//
+app.use('/api/productos', routerProd)
+app.use('/api/carrito', routerCart)
+app.use('/api/productos-test', routerProdTest)
+
+
+//****************************************************************************/
+//DESCOMENTAR PARA SUBIR LOS PRODUCTOS Y CHATS A LA BASE DE DATOS ECOMMERCE de Mongo
+// import createDocs from './db/createDocs.js'
+// createDocs();
+//****************************************************************************/
+
+
+//**********CHAT MANAGER***********************************************************************************************
+import Chat from './controllers/chat.controller.js';
+const chat = new Chat();
+
+// NORMALIZACIÓN DE MENSAJES
+import { normalize, schema, } from 'normalizr'
+// Definimos un esquema de autor
+const schemaAuthor = new schema.Entity('author', {}, { idAttribute: 'id' });
+// Esquema de mensaje
+const schemaMensaje = new schema.Entity('post', { author: schemaAuthor }, { idAttribute: '_id' })
+// Esquema de posteos
+const schemaPosteos = new schema.Entity('posts', { mensajes: [schemaMensaje] }, { idAttribute: 'id' })
+const normalizarMensajes = (mensajesConId) => normalize(mensajesConId, schemaPosteos)
+
+
+async function listNormalizedChats() {
+  //const mensajes = await chat.getMockedChats()
+  const mockMess = await chat.getAllChats() // obtengo todos los chats
+  const messToString = JSON.stringify(mockMess) // Paso a string
+  const mensajes= JSON.parse(messToString) // Paso a JSON y obtener un objeto
+  //console.log(mensajes)
+  const normalizados = normalizarMensajes({ id: 'mensajes', mensajes }) // Normalizo y le coloco un id al objeto a normalizar
+  console.log(util.inspect(normalizados,false,12,true))
+  return normalizados
+}
+
+io.on("connection", async (socket) => {
+  console.log("💻 Nuevo usuario conectado!"); //Mensajes de control
+  socket.on("disconnect", () => {
+    console.log("❌ Usuario desconectado");
+  });
+  socket.on('connect_error', (err) => {
+    console.log(`Connect_error due to ${err.message}`);
+  });
+
+  //const oldMessages = await chat.getAllChats()  //Obtener chats
+  //socket.emit("sendMessages", oldMessages)   //Enviar chats guardados al Front:
+  socket.emit("sendMessages", await listNormalizedChats())
+  
+  socket.on("sendNewChat", async (newMessage) => {  // Obtener los nuevos chats desde el front
+    newMessage.created_at = new Date().toLocaleString();
+    await chat.saveNewMessage(newMessage); //-->meterlos en la db
+    // const newChats = await chat.getAllChats()//--> volver a obtener todo
+    // io.sockets.emit("sendMessages", newChats); //==> devuelve a todos los usuarios conectados 
+    io.sockets.emit("sendMessages", await listNormalizedChats()); //==> devuelve a todos los usuarios conectados 
+  });
+})
+//*********************************************************************************************************
+
+
+app.all('*', (req, res) => { //MENSAJE PARA RUTA NO IMPLEMENTADA:
+  res.status(501).json({ error: -2, descripcion: `Ruta no implementada` })
+})
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`🚀 Server started on port http://localhost:${PORT}`),);
+server.on('error', (err) => console.log(err));
