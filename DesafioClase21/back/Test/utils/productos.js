3b172f347b59bcfe35de32c9ec3c99342f49ef3f
Luis Taillant <luis.marcelo.taillant@gmail.com>
2022-07-17 19:57:31 -0300

    DesafioClase21

---
+++ DesafioClase21/back/Test/utils/productos.js
@@ -0,0 +1,15 @@
+export const productoNuevo = {
+    nombre: 'PNotebook HP',
+    tipo: 'notebook',
+    precio: 2500,
+    imagen: 'https://www.pcfactory.cl/public/foto/42286/1.jpg',
+    stock: 10
+}
+
+export const productoAModificar = {
+    nombre: 'Smartphone',
+    tipo: 'smartphone',
+    precio: 2500,
+    imagen: 'https://www.pcfactory.cl/public/foto/42835/1.jpg',
+    stock: 10
+}
